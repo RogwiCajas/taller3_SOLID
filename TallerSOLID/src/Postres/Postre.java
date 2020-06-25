8a62c449a9832b9ad6c20195b2864e10e5a06ba4
walter santacruz <wasantac@espol.edu.ec>
2020-06-25 10:25:22 -0500

    eliminar to String

--- TallerSOLID/src/Postres/Postre.java
+++ TallerSOLID/src/Postres/Postre.java
@@ -48,11 +48,6 @@ public class Postre {
     public ArrayList<Aderezo> getAderezos() {
         return aderezos;
     }
-
-    @Override
-    public String toString() {
-        return "Helado{" + "sabor=" + sabor + ", precioParcial=" + precioParcial + ", aderezos=" + aderezos + '}';
-    }
     
     public String showPrecioFinal(){
         return "Precio Final: $ " + calcularPrecioFinal();
